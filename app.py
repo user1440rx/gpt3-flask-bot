0fc72619264093154fdaf412304a4a3b618fbb52
vshnv <109591279+user1440rx@users.noreply.github.com>
2022-07-26 19:44:58 +0530

    Updated app.py
    Flask routing: Changed static folder to point to the React page.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 import os
 import openai
 from time import time,sleep
 
+app = Flask(__name__, static_folder='static/react-chat')
 
-app = Flask(__name__)
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
@@ -12,6 +12,7 @@ def open_file(filepath):
 
 # Create an openaiapikey.txt file and save your api key.
 openai.api_key = open_file('openaiapikey.txt')
+openai.api_key = openai.api_key.rstrip('\n')
 
 
 def bot(prompt, engine='text-davinci-002', temp=0.9, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.5, stop=['<<END>>']):
@@ -42,9 +43,15 @@ def bot(prompt, engine='text-davinci-002', temp=0.9, top_p=1.0, tokens=1000, fre
             sleep(1)
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
 
 @app.route("/get")
 def get_bot_response():
@@ -52,5 +59,5 @@ def get_bot_response():
     botresponse = bot(prompt =userText)
     return str(botresponse)
 
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
